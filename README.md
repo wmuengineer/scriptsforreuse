9b7c4277b98215500e6f87a4b5cf4f99e6ff36cd
wmuenigneer <adam.d.bell@icloud.io>
2019-07-30 15:39:30 -0700

    more simple scripts

--- README.md
+++
@@ -1,2 +0,0 @@
-# scriptsforreuse
-lots of scripts I reuse for a variety of projects
--- install-ruby-2-1-2.sh
+++ install-ruby-2-1-2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 \curl -sSL https://get.rvm.io | bash
 source /usr/local/rvm/scripts/rvm
-rvm install 2.1.10
-rvm use 2.1.10
+rvm install 2.1.2
+rvm use 2.1.2
